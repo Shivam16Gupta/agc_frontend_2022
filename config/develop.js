0d6103f5391670e0d8f6b6c9946639950b732230
Shivam16Gupta <shivamgupta1281@gmail.com>
2023-05-10 18:52:25 +0530

    added config

---
+++ config/develop.js
@@ -0,0 +1,16 @@
+module.exports = {
+    ENV: 'DEVELOP',
+    BASE_URL: 'http://localhost:3000/',
+    // APIURL: 'http://localhost:8080/AgraCollege-BackEnd/',
+    APIURL: 'http://localhost/AgraCollege-BackEnd/',
+    ROLES: ['STUDENT', 'ADMIN'],
+    MERCHANTID: 'BDSKUATY',
+    SECURITYID: 'bdskuaty',
+    PAYMENTAPI: 'https://uat.billdesk.com/pgidsk/PGIMerchantPayment',
+    // RESPONSEURL: 'http://localhost:8080/AgraCollege-BackEnd/payment.php',
+    RESPONSEURL: 'http://localhost/AgraCollege-BackEnd/payment.php',
+    QUERYAPI: 'https://uat.billdesk.com/pgidsk/PGIQueryController',
+    CAPTCHA: '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI',
+    APP_VERSION: '6.8',
+    ADMIN_APP_VERSION:'5.7',
+  }
